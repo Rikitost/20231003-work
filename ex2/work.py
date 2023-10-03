d7021bb55de55cded3e424e1e4e92d116bcd9139
Rikitost <kd1300674@st.kobedenshi.ac.jp>
2023-10-03 12:22:49 +0900

    ex2に運賃計算をするコードを追加

--- ex2/work.py
+++ ex2/work.py
@@ -1,8 +1,30 @@
 from math import ceil
+import math
+
+"""
+タクシーの運賃計算
+    タクシーの運賃を計算するコードを作成せよ
+    * 初乗り運賃は 1700m までは610円とする
+    * 1700mを超えるとその後は 315m毎に 80円増えるものとする
+    * 引数として走行距離(整数でm単位)が渡されるものとする
+    * 戻り値は金額(整数値)とする
+    * 1mでも超えれば80円単位でかかるものとする
+    * 0mおよびマイナスの場合はNoneを返す
+"""
 
 def calc_account(m):
     # 実装は入れていません、自分で入れてください
-    pass
+    # 0mおよびマイナスの場合はNoneを返す
+    if m <= 0 :
+        return None
+    # 初乗り運賃は 1700m までは610円とする
+    # 1700mを超えるとその後は 315m毎に 80円増えるものとする
+    if m <= 1700 :
+        result = 610
+    else :
+        count = math.ceil((m - 1700) / 315)
+        result = 610 + count * 80
+    return result
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
